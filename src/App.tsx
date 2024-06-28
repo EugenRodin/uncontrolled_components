3b1773983bdb5ba610f6346c79b1ffc447a8d719
EugenRodin <gameover59630@gmail.com>
2024-06-28 12:35:16 +0200

    uncontrolled

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LoginForm from './components/LoginForm.tsx'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Uncontrolled</h1>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default App
