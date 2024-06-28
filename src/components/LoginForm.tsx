162d44b0ce31806a2fd748ef81b625f08b4ca490
EugenRodin <gameover59630@gmail.com>
2024-06-28 12:47:01 +0200

    uncontrolled-via-useRef

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,24 +1,24 @@
-import { FormEvent } from 'react'
+import { FormEvent, useRef } from 'react'
 
 const LoginForm = () => {
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const target = e.target as typeof e.target & {
-      username: { value: string }
-      password: { value: string }
-    }
+    
 
-    console.log(target.username.value)
-    console.log(target.password.value)
+    console.log("Username:", usernameRef.current?.value)
+    console.log("Password:", passwordRef.current?.value)
   }
 
   return (
     <form className="col-6" onSubmit={handleFormSubmit}>
       <div className="mb-3">
-        <input type="text" className="form-control" name="username" placeholder="Username..." />
+        <input ref={usernameRef} type="text" className="form-control" name="username" placeholder="Username..." />
       </div>
       <div className="mb-3">
-        <input type="text" className="form-control" name="password" placeholder="Password..." />
+        <input ref={passwordRef} type="text" className="form-control" name="password" placeholder="Password..." />
       </div>
       <button type="submit" className="btn btn-primary">
         Login
